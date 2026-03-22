026318db9e2a7c67ec230ea5e88a28523ff320b7
Efe Akpinar <efeakpinar0@hotmail.com>
2026-03-23 01:12:48 +0300

    Initial project setup: Spring Boot backend + React frontend

---
+++ backend/src/test/java/com/_8/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com._8.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
